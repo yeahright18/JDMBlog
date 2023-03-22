4a06ccd053035bbe8b7e9df6a0e89379d9aa1edf
yeahright18 <szymonsk312@gmail.com>
2023-03-22 19:45:25 +0100

    Add edit categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -24,6 +24,20 @@ class CategoriesController < ApplicationController
     @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Kategoria zaktualizowana"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def require_admin
